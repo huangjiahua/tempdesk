af7808cb03e18b804e99ada53117fea53b6c9208
huangjiahua <jiahua.h@outlook.com>
2020-07-20 22:23:16 +0800

    implement the AddUser API

--- internal/http/handler/user.go
+++ internal/http/handler/user.go
@@ -3,9 +3,11 @@ package handler
 import (
 	"bytes"
 	"encoding/json"
+	td "github.com/huangjiahua/tempdesk"
 	thttp "github.com/huangjiahua/tempdesk/internal/http"
 	tlog "github.com/huangjiahua/tempdesk/internal/log"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -13,13 +15,7 @@ type User struct {
 	state *thttp.State
 }
 
-func (u *User) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
-	if err != nil {
-		tlog.Debug("error parsing url", tlog.String("err", err.Error()))
-		http.Error(res, "Error parsing url arguments or form", http.StatusBadRequest)
-		return
-	}
+func (u *User) ServeGetUser(res http.ResponseWriter, req *http.Request) {
 	user, err := u.state.AuthUser(req)
 	if err != nil {
 		tlog.Debug("error authenticating", tlog.String("err", err.Error()))
@@ -39,4 +35,64 @@ func (u *User) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		tlog.Warn("error writing to response", tlog.String("err", err.Error()))
 	}
+}
+
+func (u *User) ServeAddUser(res http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		tlog.Debug("error parsing body", tlog.String("err", err.Error()))
+		http.Error(res, "error parsing body", http.StatusBadRequest)
+		return
+	}
+	info, err := parseUserSignUpInfo(body)
+	if err != nil {
+		tlog.Debug("error parsing submitted json", tlog.String("err", err.Error()))
+		http.Error(res, "error parsing submitted json", http.StatusBadRequest)
+		return
+	}
+	user := td.User{
+		Name: info.Name,
+		Key:  info.Key,
+		Meta: info.Meta,
+	}
+
+	err = u.state.Users.CreateUser(user)
+	if err != nil {
+		// TODO: handle each kind of error
+		tlog.Debug("error creating new user", tlog.String("err", err.Error()))
+		http.Error(res, "error creating new user", http.StatusBadRequest)
+		return
+	}
+	res.WriteHeader(http.StatusOK)
+}
+
+func (u *User) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		tlog.Debug("error parsing url", tlog.String("err", err.Error()))
+		http.Error(res, "Error parsing url arguments or form", http.StatusBadRequest)
+		return
+	}
+
+	switch req.Method {
+	case http.MethodGet:
+		u.ServeGetUser(res, req)
+	case http.MethodPost:
+		u.ServeAddUser(res, req)
+	default:
+		tlog.Debug("unsupported method", tlog.String("method", req.Method))
+		http.Error(res, "method not supported", http.StatusMethodNotAllowed)
+	}
+}
+
+type userSignUpInfo struct {
+	Name string            `json:"name"`
+	Key  string            `json:"password"`
+	Meta map[string]string `json:"meta,omitempty"`
+}
+
+func parseUserSignUpInfo(data []byte) (userSignUpInfo, error) {
+	var i userSignUpInfo
+	err := json.Unmarshal(data, &i)
+	return i, err
 }
--- user.go
+++ user.go
@@ -1,12 +1,26 @@
 package tempdesk
 
+const (
+	NameAlreadyExist string = "Name Already Exist"
+)
+
 type User struct {
 	ID   int
 	Name string
 	Key  string
+	Meta map[string]string
 }
 
 type UserService interface {
 	User(name string) (user User, ok bool)
 	CreateUser(user User) (err error)
+}
+
+type UserServiceError struct {
+	Kind string
+	Err  error
+}
+
+func (u *UserServiceError) Error() string {
+	return u.Kind
 }
