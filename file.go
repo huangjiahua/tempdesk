926c7c6c6f49e27f38aae3c3f6fa30f6d1c98ef2
huangjiahua <jiahua.h@outlook.com>
2020-08-12 15:18:50 +0800

    define file interface

--- file.go
+++ file.go
@@ -1,16 +1,35 @@
 package tempdesk
 
-type FileMeta map[string]string
+import "io"
 
-type FileData interface {
+type FilePermission struct {
+	owner       string
+	exclusive   bool
+	notAllowed  map[string]bool
+	allowed     map[string]bool
+	allowedMeta map[string]string
 }
 
-type File struct {
-	Path string
-	Name string
-	Meta FileMeta
-	Data FileData
+type File interface {
+	io.Reader
+	io.Writer
+	io.Seeker
+	io.ReaderAt
+	io.WriterAt
+
+	Perm() *FilePermission
+
+	Meta(key string) (value string)
+	WriteMeta(key string, value string) (err error)
+	FileMeta(key string) (value interface{})
+	WriteFileMeta(key string, value interface{}) (err error)
+
+	Truncate(pos int64, data []byte) (err error)
 }
 
 type FileService interface {
+	File(path string) (err error)
+	Open(path string, flags int, perm *FilePermission) (file *File, err error)
+	Rename(dest string, src string) (err error)
+	Remove(path string) (err error)
 }
--- internal/mock/file.go
+++ internal/mock/file.go
@@ -1,6 +1,26 @@
 package mock
 
+import (
+	td "github.com/huangjiahua/tempdesk"
+)
+
 type FileService struct {
+}
+
+func (fs FileService) File(path string) (err error) {
+	panic("implement me")
+}
+
+func (fs FileService) Open(path string, flags int, perm *td.FilePermission) (file *td.File, err error) {
+	panic("implement me")
+}
+
+func (fs FileService) Rename(dest string, src string) (err error) {
+	panic("implement me")
+}
+
+func (fs FileService) Remove(path string) (err error) {
+	panic("implement me")
 }
 
 func NewFileService() *FileService {
